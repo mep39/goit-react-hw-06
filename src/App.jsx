c6c978ced45ff040e52ad90c20d8a60da49de167
mepinit <evgenmep@gmail.com>
2024-04-07 21:21:49 +0300

    hw6

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import ContactForm from "./ContactForm/ContactForm";
+import ContactList from "./ContactList/ContactList";
+import SearchBox from "./SearchBox/SearchBox";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+  );
+}
